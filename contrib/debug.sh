8b00b50a796f9fcc22b0377e95eaa13aa387cb9a
Jose Ricardo Ziviani <jose@ziviani.net>
2021-01-04 09:56:39 -0300

    Some improvements on AHCI

--- contrib/debug.sh
+++ contrib/debug.sh
@@ -2,13 +2,14 @@
 set -x
 readonly DIR="$1"
 
-[[ ! -f hda.qcow2 ]] && qemu-image create hda.qcow2 -f qcow2 256M
+[[ ! -f hda.img ]] && qemu-image create hda.img -f raw 128M
+mkfs.ext2 hda.img
 
 qemu-system-x86_64                                                          \
     -m 1G -machine q35,accel=tcg                                            \
     -cdrom "$DIR/xenon.iso"                                                 \
     -device ide-hd,bus=ide.0,drive=drive-sata0-0-0,id=sata0-0-0,bootindex=1 \
-    -drive file=hda.qcow2,format=qcow2,if=none,id=drive-sata0-0-0           \
+    -drive file=hda.img,format=raw,if=none,id=drive-sata0-0-0               \
     -device piix3-usb-uhci,id=usb,bus=pcie.0,addr=0xc                       \
     -vga std                                                                \
     -s -S -d cpu_reset & disown
